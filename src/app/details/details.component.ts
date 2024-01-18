a875c794f89d99a48c65a421626ec79af187ff3a
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-18 17:10:07 +0400

    c

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -9,7 +9,7 @@ import { HousingLocation } from '../housinglocation';
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
     <article>
       <img class="listing-photo" [src]="housingLocation?.photo">
@@ -29,7 +29,15 @@ import { HousingLocation } from '../housinglocation';
       </section>
       <section>
         <h2 class="section-heading">Apply to live here</h2>
-        <button class="primary">Apply NOW</button>
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">First Name</label>
+          <input type="text" id="first-name" formControlName="firstName">
+          <label for="last-name">Last Name</label>
+          <input type="text" id="last-name" formControlName="lastName">
+          <label for="email">Email</label>
+          <input type="email" id="email" formControlName="email">
+          <button type="submit" class="primary">Apply now</button>
+      </form>
       </section>
     </article> 
 
@@ -41,8 +49,25 @@ export class DetailsComponent {
   housingService = inject(HousingService)
   housingLocation: HousingLocation | undefined
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  })
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id'])
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId)
+    this.housingService.getHousingLocationById(housingLocationId)
+      .then(housingLocation => {
+        this.housingLocation = housingLocation
+      })
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    )
   }
 }
